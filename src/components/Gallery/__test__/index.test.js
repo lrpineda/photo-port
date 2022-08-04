9a398f723f5a6cde8a30a92926ef2d99ba895209
lrpineda <luicks212@gmail.com>
2022-08-04 00:26:47 -0600

    added gallery tests

---
+++ src/components/Gallery/__test__/index.test.js
@@ -0,0 +1,20 @@
+import { render, cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Gallery from '..';
+
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
+
+afterEach(cleanup);
+
+describe('Gallery is rendering', () => {
+    it('renders', () => {
+        const { getByTestId } = render(<Gallery currentCategory={portrait} />);
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        expect(getByTestId('h1tag')).toHaveTextContent('Portraits');
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Gallery currentCategory={portrait} />)
+        expect(asFragment()).toMatchSnapshot()
+      })
+});
